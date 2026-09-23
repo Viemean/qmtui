19db4a189ed14be6b09d6506d7ceccd2e204d208
Yuzuki <lxf74663@gmail.com>
2026-09-24 06:43:41 +0800

    chore(release): bump version to 0.3.10

--- PKGBUILD
+++ PKGBUILD
@@ -2,7 +2,7 @@
 
 pkgname=qmtui-bin
 _pkgname=qmtui
-pkgver=0.3.9
+pkgver=0.3.10
 pkgrel=1
 pkgdesc="Linux terminal qqmusic player (.NET 10 Native AOT pre-built binary package)"
 arch=('x86_64' 'aarch64')
--- src/Program.cs
+++ src/Program.cs
@@ -13,7 +13,7 @@ public static partial class Program
     {
         if (args.Contains("--version") || args.Contains("-v"))
         {
-            Console.WriteLine("qmtui 0.3.9");
+            Console.WriteLine("qmtui 0.3.10");
             return;
         }
 
