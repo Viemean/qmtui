d23598400dd4138217ccc7d97c066d26094dc808
Yuzuki <lxf74663@gmail.com>
2026-09-25 05:41:00 +0800

    chore(release): bump version to 0.3.12

--- src/Program.cs
+++ src/Program.cs
@@ -13,7 +13,7 @@ public static partial class Program
     {
         if (args.Contains("--version") || args.Contains("-v"))
         {
-            Console.WriteLine("qmtui 0.3.11");
+            Console.WriteLine("qmtui 0.3.12");
             return;
         }
 
